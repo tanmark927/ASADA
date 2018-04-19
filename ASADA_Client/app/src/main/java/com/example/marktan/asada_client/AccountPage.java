24e659d4524d00db475051402eaa61932ae94d5c
Mark Tan <damtan1996@gmail.com>
2018-04-18 20:20:45 -0700

    implemented bar chart
    info was manually added
    will have to figure out how to do it based on Db info

--- ASADA_Client/app/src/main/java/com/example/marktan/asada_client/AccountPage.java
+++ ASADA_Client/app/src/main/java/com/example/marktan/asada_client/AccountPage.java
@@ -1,13 +1,18 @@
 package com.example.marktan.asada_client;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import java.util.*;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.BarData;
+
 
 public class AccountPage extends AppCompatActivity {
+
+    BarChart barChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,19 @@ public class AccountPage extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        barChart = (BarChart) findViewById(R.id.bargraph);
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(10f,4));
+        entries.add(new BarEntry(20f,2));
+        entries.add(new BarEntry(30f,5));
+        entries.add(new BarEntry(40f,3));
+        BarDataSet bds = new BarDataSet(entries,"dates");
+
+        BarData bd = new BarData(bds);
+        barChart.setData(bd);
+        barChart.setTouchEnabled(true);
+        barChart.setDragEnabled(true);
+        barChart.setScaleEnabled(true);
     }
 
 }
