06b62dc64d1454a912d94bf1e275bf8c9fb202bd
hungmch95 <proxvalkryne@gmail.com>
2018-04-23 15:20:02 -0700

    updated with logins to RDS server

--- Alexa Code/lambda_function.py
+++ Alexa Code/lambda_function.py
@@ -1,5 +1,25 @@
 from __future__ import print_function
+import sys
+import logging
+import pymysql
 
+#rds settings
+rds_host  = "asada.cofr9vg9xjlm.us-east-1.rds.amazonaws.com"
+name = "asadadepression"
+password = "Dontbesad1"
+db_name = "asadaDB"
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+try:
+    conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+except:
+    logger.error("ERROR: Unexpected error: Could not connect to MySql instance.")
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS mysql instance succeeded")
 
 # --------------- Helpers that build all of the responses ----------------------
 
