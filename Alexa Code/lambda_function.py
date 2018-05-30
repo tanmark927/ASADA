a613c20319db0c1410940ef08f9ad21c27cce585
Mark Tan <damtan1996@gmail.com>
2018-05-30 15:10:04 -0700

    begin survey logic for Lambda, removed old temp methods

--- Alexa Code/lambda_function.py
+++ Alexa Code/lambda_function.py
@@ -15,7 +15,7 @@ db_name = "asadaDB"
 
 #-------variables for survey-------
 
-QUESTION_COUNT = 10
+QUESTION_COUNT = 9
 
 OPENING_MESSAGE = "This is the Patient Health Questionnaire. " \
                    "It will measure the severity and presence of depression. " \
@@ -45,6 +45,18 @@ BREAKSTRONG = "<break strength='strong'/>"
 #several days - 1
 #more than half - 2
 #nearly every day - 3
+
+PROBLEMS = []
+PROBLEMS.append("Little interest or pleasure in doing things")
+PROBLEMS.append("Feeling down, depressed or hopeless")
+PROBLEMS.append("Trouble falling asleep, staying asleep, or sleeping too much")
+PROBLEMS.append("Feeling tired or having little energy")
+PROBLEMS.append("Poor appetite or overeating")
+PROBLEMS.append("Feeling bad about yourself")
+PROBLEMS.append("Trouble concentrating")
+PROBLEMS.append("Moving or speaking too slowly or quickly")
+PROBLEMS.append("Suicidal thoughts")
+
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -121,7 +133,6 @@ def get_welcome_response():
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
-
 def handle_session_end_request():
     card_title = "Session Ended"
     speech_output = "Thank you for using ASADA. " \
@@ -130,70 +141,6 @@ def handle_session_end_request():
     should_end_session = True
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
-
-'''
-#TODO FOR ASADA: change intents to be appropriate with our functions
-def create_favorite_color_attributes(favorite_color):
-    return {"favoriteColor": favorite_color}
-
-#TODO FOR ASADA: change intents to be appropriate with our functions
-def AdviceGiverAction(intent, session):
-    
-    card_title = intent['name']
-    session_attributes = {}
-    should_end_session = False
-
-    #if 'Color' in intent['slots']:
-    #    favorite_color = intent['slots']['Color']['value']
-    #    session_attributes = create_favorite_color_attributes(favorite_color)
-    #    speech_output = "I now know your favorite color is " + \
-    #                    favorite_color + \
-    #                    ". You can ask me your favorite color by saying, " \
-    #                    "what's my favorite color?"
-    #    reprompt_text = "You can ask me your favorite color by saying, " \
-    #                    "what's my favorite color?"
-    #else:
-    #    speech_output = "I'm not sure what your favorite color is. " \
-    #                    "Please try again."
-    #    reprompt_text = "I'm not sure what your favorite color is. " \
-    #                    "You can tell me your favorite color by saying, " \
-    #                    "my favorite color is red."
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
-
-#TODO FOR ASADA: change intents to be appropriate with our functions, may actually delete this one
-def ContinueConvoAction(intent, session):
-    session_attributes = {}
-    reprompt_text = None
-
-    #if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
-    #    favorite_color = session['attributes']['favoriteColor']
-    #    speech_output = "Your favorite color is " + favorite_color + \
-    #                    ". Goodbye."
-    #    should_end_session = True
-    #else:
-    #    speech_output = "I'm not sure what your favorite color is. " \
-    #                    "You can say, my favorite color is red."
-    #    should_end_session = False
-
-    # Setting reprompt_text to None signifies that we do not want to reprompt
-    # the user. If the user does not respond or says something that is not
-    # understood, the session will end.
-    return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output, reprompt_text, should_end_session))
-
-
-def MusicAction(intent, session):
-    # Setting reprompt_text to None signifies that we do not want to reprompt
-    # the user. If the user does not respond or says something that is not
-    # understood, the session will end.
-    return build_response(session_attributes, build_speechlet_response(
-        intent['name'], speech_output, reprompt_text, should_end_session))
-    
-#TODO: fill out logic for pause and resume intent
-def do_something():
-    return 0;
-'''
 
 # --------------- Events ------------------
 
@@ -255,6 +202,29 @@ def fortune_cookie():
         return build_response(session_attributes, build_speechlet_response(
                 card_title, speech_output, reprompt_text, should_end_session))
     
+def ask_question(request, speech_output):
+    #reset COUNTER
+    if globals()['COUNTER'] <= 0:
+        globals()['COUNTER'] = 0
+        speech_output = OPENING_MESSAGE
+    
+    globals()['COUNTER'] += 1
+    
+    item_cls = PROBLEMS[COUNTER - 1]
+    quiz_property = item_cls.get_prop() #TODO: Create method
+    reprompt_text = get_question(quiz_property, item_cls) #TODO: create method
+    speech_output += reprompt_text
+    card_title = "Question" + str(COUNTER)
+    session_attributes = {"quizscore":globals()['QUIZSCORE'],
+                  "quizproperty":quiz_property,
+                  "response":speech_output,
+                  "state": globals()['STATE'],
+                  "counter":globals()['COUNTER'],
+                  "quizitem":item_cls.__dict__
+                 }
+    should_end_session = False
+        return build_response(session_attributes, build_speechlet_response(
+                card_title, speech_output, reprompt_text, should_end_session))    
 
 def do_quiz():
     session_attributes = {}
